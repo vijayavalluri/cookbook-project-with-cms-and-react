b373b0eaf2285f7886e89a2500b6c8a6241c9bc1
vijayavalluri <vvvijaya56@gmail.com>
2023-08-07 16:21:17 +0200

    add reciepe details to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import { useRef, useEffect, useState } from 'react';
 import {motion} from 'framer-motion';
 import useContentful from "./useContentful";
 
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+// import componentDetails from 'componentDetails.jsx';
+
 
 function App() {
+{/* <Link to={{pathname: '/details', 
+         state: [{title: {title}, description:{description}, img:{img}}]}}> Your details
+</Link>  */}
+  // const navigate = useNavigate();
+  // connst toComponentDetails=()=>{
+  //    avigate('/componentDetails',{state:{title: {title}, description:{description}, img:{img}}});
+  //      }
+
   const [foods, setFoods] = useState();
   const { getFoods } = useContentful();
   const [width, setWidth] = useState(0)
@@ -27,9 +39,16 @@ function App() {
   }, [carousel.current]); // Re-run whenever the carousel ref changes
   
   
-  
   return (
+    <Router>
+    <Switch>
+      <Route path="/recipe/:id" component={RecipeDetail}/>
+      <Route path="/" exact>
+
+    
     <div className="App">
+    
+
       <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
         <motion.div 
           drag="x" 
@@ -37,11 +56,13 @@ function App() {
           className="inner-carousel"
         >
           {!foods ? (
-            <p>loading</p>
+            <p>loading</p>  
           ) : (
             foods.map((food) => {
               return(
-<motion.div className="item" key={food.title}>
+                <Link to={/recipe/${food.id}} key={food.id}}>
+                  
+<motion.div className="item">
     <div className="item-wrap">
     <h3 
   className="title" 
@@ -56,12 +77,18 @@ function App() {
         <img src={food.img} alt={food.title} />
     </div>
 </motion.div>
+</Link>
               )
             })
           )}
         </motion.div>
       </motion.div>
+      {/* <button> <a onClick={()=>navigate(-1)}>Go Back</a></button>
+      <button> <a onClick={()=>navigate(1)}>Go Forward </a></button> */}
     </div>
+    </Route>
+    </Switch>
+    </Router>
   );
 }
 
