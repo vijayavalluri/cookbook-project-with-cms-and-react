03ff9699e84546a85758a09f27d74c726157d383
blkjacobinsjr <124602974+blkjacobinsjr@users.noreply.github.com>
2023-08-07 17:15:01 +0200

    RecipeDetails page connected to vijaya route

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,11 @@ import './App.scss';
 import { useRef, useEffect, useState } from 'react';
 import {motion} from 'framer-motion';
 import useContentful from "./useContentful";
-
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom';
-// import componentDetails from 'componentDetails.jsx';
-
+import RecipeDetail from './RecipeDetail';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
-{/* <Link to={{pathname: '/details', 
-         state: [{title: {title}, description:{description}, img:{img}}]}}> Your details
-</Link>  */}
-  // const navigate = useNavigate();
-  // connst toComponentDetails=()=>{
-  //    avigate('/componentDetails',{state:{title: {title}, description:{description}, img:{img}}});
-  //      }
-
-  const [foods, setFoods] = useState();
+const [foods, setFoods] = useState([]);
   const { getFoods } = useContentful();
   const [width, setWidth] = useState(0)
   const carousel = useRef();
@@ -37,59 +26,46 @@ function App() {
       setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
     }
   }, [carousel.current]); // Re-run whenever the carousel ref changes
-  
-  
-  return (
-    <Router>
-    <Switch>
-      <Route path="/recipe/:id" component={RecipeDetail}/>
-      <Route path="/" exact>
 
-    
-    <div className="App">
-    
-
-      <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
-        <motion.div 
-          drag="x" 
-          dragConstraints={{ right: 0, left: -width }}
-          className="inner-carousel"
-        >
-          {!foods ? (
-            <p>loading</p>  
-          ) : (
-            foods.map((food) => {
-              return(
-                <Link to={/recipe/${food.id}} key={food.id}}>
-                  
-<motion.div className="item">
-    <div className="item-wrap">
-    <h3 
-  className="title" 
-  onMouseDown={(e) => {
-    e.stopPropagation();
-    console.log('Title Mouse Down'); // add this line
-  }}
->
-  {food.title}
-</h3>
-
-        <img src={food.img} alt={food.title} />
-    </div>
-</motion.div>
-</Link>
-              )
-            })
-          )}
+    return (
+<Router>
+  <Routes>
+    <Route path="/" element={
+      <div className="App">
+        <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
+          <motion.div 
+            drag="x" 
+            dragConstraints={{ right: 0, left: -width }}
+            className="inner-carousel"
+          >
+            { foods.map((food) => {
+                return(
+                  <Link to={`/recipe/${food.id}`} key={food.id}>                  
+                    <motion.div className="item">
+                      <div className="item-wrap">
+                        <h3 
+                          className="title" 
+                          onMouseDown={(e) => {
+                            e.stopPropagation();
+                            console.log('Title Mouse Down'); // add this line
+                          }}
+                        >
+                          {food.title}
+                        </h3>
+                        <img src={food.img} alt={food.title} />
+                      </div>
+                    </motion.div>
+                  </Link>
+                )
+              })}
+          </motion.div>
         </motion.div>
-      </motion.div>
-      {/* <button> <a onClick={()=>navigate(-1)}>Go Back</a></button>
-      <button> <a onClick={()=>navigate(1)}>Go Forward </a></button> */}
-    </div>
-    </Route>
-    </Switch>
-    </Router>
-  );
-}
+      </div>
+    } />
+      <Route path="/recipe/:id" element={<RecipeDetail />} />
+  </Routes>
+</Router>
+    )}
 
-export default App;
+
+export default App
